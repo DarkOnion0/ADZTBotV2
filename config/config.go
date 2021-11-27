8150f94a0cd85288d78c3c2c0e88a390e651c0ba
DarkOnion0 <Darkgenius1@protonmail.com>
2021-11-27 14:41:52 +0100

    centralize configuration in config.go

--- config/config.go
+++ config/config.go
@@ -1,13 +1,19 @@
 package config
 
 import (
-	"os"
+	"flag"
 
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
+func init() {
+	flag.Parse()
+}
+
 var (
-	Client *mongo.Client
-	DBName = os.Getenv("ADZTBotV2_DB_NAME")
-	DBUrl  = os.Getenv("ADZTBotV2_DB_URL")
+	Client   *mongo.Client
+	DBName   = flag.String("db", "", "The mongodb database name")
+	DBUrl    = flag.String("url", "", "The mongodb access url")
+	GuildID  = flag.String("guild", "", "Test guild ID. If not passed - bot registers commands.Commands globally")
+	BotToken = flag.String("token", "", "Bot access token")
 )
