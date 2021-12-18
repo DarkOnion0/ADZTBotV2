2c77a00da7e1db73a1b9a1f9859480eee6378052
DarkOnion0 <Darkgenius1@protonmail.com>
2021-12-18 22:45:24 +0100

    add the vote function in post.go

--- db/post.go
+++ db/post.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"context"
+	"errors"
 	"log"
 	"strings"
 	"time"
@@ -12,8 +13,16 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
-type postRecord struct {
+type postRecordSendT struct {
 	//ID       primitive.ObjectID `bson:"_id" json:"id,omitempty"`
+	Type     string
+	Url      string
+	User     primitive.ObjectID
+	VoteList []postVote
+}
+
+type postRecordFetchT struct {
+	ID       primitive.ObjectID `bson:"_id" json:"id,omitempty"`
 	Type     string
 	Url      string
 	User     primitive.ObjectID
@@ -25,10 +34,15 @@ type postVote struct {
 	Vote string
 }
 
+// The Post function check if post exist in the database according to his link.
+//
+// 1. If it not exists the post will be added and the function will return (true, OBJECTID)
+//
+// 2. Else it will return (false, "")
 func Post(userDbId primitive.ObjectID, postType, postUrl string) (bool, string) {
 	postCollection := config.Client.Database(*config.DBName).Collection("post")
 
-	var postRecordFetch postRecord
+	var postRecordFetch postRecordFetchT
 	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
 	err1 := postCollection.FindOne(ctx, bson.D{{"url", strings.Split(postUrl, "?si=")[0]}}).Decode(&postRecordFetch)
 
@@ -37,11 +51,9 @@ func Post(userDbId primitive.ObjectID, postType, postUrl string) (bool, string) 
 	if err1 != nil {
 		if err1 == mongo.ErrNoDocuments {
 			// add new post to the db
-			postCollection := config.Client.Database(*config.DBName).Collection("post")
-
 			ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
 
-			info, _ := postCollection.InsertOne(ctx, postRecord{Type: postType, Url: strings.Split(postUrl, "?si=")[0], User: userDbId, VoteList: []postVote{{User: userDbId, Vote: "+"}}})
+			info, _ := postCollection.InsertOne(ctx, postRecordSendT{Type: postType, Url: strings.Split(postUrl, "?si=")[0], User: userDbId, VoteList: []postVote{{User: userDbId, Vote: "+"}}})
 			log.Printf("A new user post has been added to the database; userDbId=%s url=%s type=%s dbId=%s", userDbId, postUrl, postType, info.InsertedID.(primitive.ObjectID).Hex())
 			return false, info.InsertedID.(primitive.ObjectID).Hex()
 		}
@@ -52,4 +64,63 @@ func Post(userDbId primitive.ObjectID, postType, postUrl string) (bool, string) 
 	}
 
 	return true, ""
+}
+
+// The Vote function add or remove a like to post (set with the Post function)
+//
+// NOTE: it will return true if the post has been added and false if not
+func Vote(postId, userVote string, userId primitive.ObjectID) (error, bool) {
+
+	postCollection := config.Client.Database(*config.DBName).Collection("post")
+	postIdPrimitive, err1 := primitive.ObjectIDFromHex(postId)
+
+	if err1 != nil {
+		log.Fatalf("An error occured while convertign the post hex ObjectID to the primitve.ObjectID")
+	}
+
+	var postRecordFetch postRecordFetchT
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err2 := postCollection.FindOne(ctx, bson.D{{"_id", postIdPrimitive}}).Decode(&postRecordFetch)
+
+	//fmt.Println(err2, strings.Split(postUrl, "?si=")[0])
+
+	if err2 != nil {
+		if err2 == mongo.ErrNoDocuments {
+			return errors.New("the postID is not valid"), false
+		}
+		log.Fatalf("An error occured while fetching the post: %s", err2)
+		//return errors.New("An error occurred while fetching the post"), false
+	} else {
+		alreadyVote := false
+
+		for i := 0; i < len(postRecordFetch.VoteList); i++ {
+			if postRecordFetch.VoteList[i].User == userId {
+				alreadyVote = true
+				postRecordFetch.VoteList[i].Vote = userVote
+			}
+		}
+
+		if !alreadyVote {
+			// add new post to the db
+			ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+
+			_, _ = postCollection.UpdateOne(ctx, bson.M{"_id": postIdPrimitive}, bson.D{
+				{"$set", bson.D{{"voteList", append(postRecordFetch.VoteList, postVote{User: userId, Vote: userVote})}}},
+			})
+			log.Printf("Add a vote; postId=%s userVote=%s userId=%s", postId, userVote, userId.Hex())
+
+			return nil, true
+		} else {
+			ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+
+			_, _ = postCollection.UpdateOne(ctx, bson.M{"_id": postIdPrimitive}, bson.D{
+				{"$set", bson.D{{"voteList", postRecordFetch}}},
+			})
+			log.Printf("Update a vote; postId=%s userVote=%s userId=%s", postId, userVote, userId.Hex())
+
+			return nil, false
+		}
+	}
+
+	return errors.New("the function shouldn't arrive there"), false
 }
