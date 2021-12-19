aa8dad8d323e21d5b9b4df123c1328acd32e7f0b
DarkOnion0 <Darkgenius1@protonmail.com>
2021-12-19 15:29:30 +0100

    add GetUser function in post.go

--- db/user.go
+++ db/user.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"log"
 	"time"
@@ -9,6 +10,7 @@ import (
 	"ADZTBotV2/config"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 // This is the datastructures of every mongodb record in the userInfo collection
@@ -29,7 +31,7 @@ func CheckUser(userId string) (bool, primitive.ObjectID) {
 	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
 	_ = userInfoCollection.FindOne(ctx, bson.D{{"userid", userId}}).Decode(&userList)
 
-	fmt.Println(userList.Userid, userId)
+	//fmt.Println(userList.Userid, userId)
 
 	if len(userList.Userid) == 0 {
 		log.Printf("User %s doesn't exist in the database", userId)
@@ -52,4 +54,27 @@ func RegisterUser(userId string) {
 		fmt.Println(info, userId)
 		log.Printf("A new user has been added to the database; userid=%s dbId=%s", userId, info.InsertedID)
 	}
+}
+
+func GetUser(userDbId primitive.ObjectID) (err error, userId string) {
+	userInfoCollection := config.Client.Database(*config.DBName).Collection("userInfo")
+
+	var userList userRecordFetch
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err1 := userInfoCollection.FindOne(ctx, bson.D{{"_id", userDbId}}).Decode(&userList)
+
+	//fmt.Println(userList.Userid, userId)
+
+	if err1 != nil {
+		if err1 == mongo.ErrNoDocuments {
+			return errors.New("user doesnt exists in the database"), ""
+		}
+		log.Fatalf("An error occured while fetching the userId: %s", err1)
+		//return errors.New("An error occurred while fetching the post"), false
+	} else {
+		log.Printf("User %s already exists in the database", userId)
+		return nil, userList.Userid
+	}
+
+	return errors.New("the function shouldn't arrive there"), ""
 }
