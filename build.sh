a55c6634ebc2d2dc063d3261d12477f973dda774
DarkOnion0 <Darkgenius1@protonmail.com>
2021-12-28 11:47:54 +0100

    update build script for darwin

--- build.sh
+++ build.sh
@@ -25,6 +25,7 @@ fi
 ## Cross-compile ADZTBotV2 && ZIP the output ##
 ###############################################
 
+mkdir "bin"
 cd ./bin/
 
 for os in linux windows darwin
@@ -41,6 +42,15 @@ do
 			sha256sum adztbotv2_$os-$arch-$VERSION > adztbotv2_$os-$arch-$VERSION-sha256sum.txt
 			zip adztbotv2_$os-$arch-$VERSION adztbotv2_$os-$arch-$VERSION adztbotv2_$os-$arch-$VERSION-sha256sum.txt
 		done
+  elif [ $os == darwin ]; then
+    echo -e "\n$red${bold}Building $os binary...${bold}$stop_color"
+    echo -e "$red${bold}===========================${bold}$stop_color"
+
+    arch=amd64
+    echo "${bold}$os/$arch...${bold}"
+    env GOOS=$os GOARCH=$arch go build -o adztbotv2_$os-$arch-$VERSION ./../main.go
+    sha256sum adztbotv2_$os-$arch-$VERSION > adztbotv2_$os-$arch-$VERSION-sha256sum.txt
+    zip adztbotv2_$os-$arch-$VERSION adztbotv2_$os-$arch-$VERSION adztbotv2_$os-$arch-$VERSION-sha256sum.txt
 	else
 		echo -e "\n$red${bold}Building $os binary...${bold}$stop_color"
 		echo -e "$red${bold}===========================${bold}$stop_color"
