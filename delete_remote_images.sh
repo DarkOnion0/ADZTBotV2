9360484f5cacf1b3ce6e7c65388ff6b4cd96ecf1
DarkOnion0 <Darkgenius1@protonmail.com>
2022-04-14 18:18:33 +0200

    fix typo

--- delete_remote_images.sh
+++ delete_remote_images.sh
@@ -6,19 +6,32 @@ set -e
 # Simple script to remove dangling images from GHCR.
 # You need to have installed jq for this script to work properly
 container="adztbotv2"
-temp_file="ghcr_prune.ids"
+temp_file_delete="ghcr_prune_delete.ids"
+temp_file_exclude="ghcr_prune_exclude.ids"
 
-rm -rf $temp_file
+rm -rf $temp_file_exclude
+rm -rf $temp_file_delete
 
 echo "Fetching dangling images from GHCR..."
-curl -u $auth -H "Accept: application/vnd.github.v3+json" https://api.github.com/user/packages/container/$container/versions > $temp_file
+curl -u $auth -H "Accept: application/vnd.github.v3+json" https://api.github.com/user/packages/container/$container/versions > $temp_file_exclude
 
-ids_to_delete=$(cat "$temp_file" | jq -r '.[] | select(.metadata.container.tags==[]) | .id')
+ids_to_delete=$(cat "$temp_file_delete" | jq -r '.[] | select(.metadata.container.tags==[]) | .id')
 
 if [ "${ids_to_delete}" = "" ]
 then
 	echo "There are no dangling images to remove for this package"
 	exit 0
+fi
+
+ids_to_exclude=$(cat "$temp_file_exclude" | jq -r '.[] | select(.metadata.container.tags!=[]) | .name')
+
+while read -r line; do
+
+done <<< $ids_to_exclude
+
+if [ "${ids_to_exclude}" = "" ]
+then
+	echo "There are no dangling images to exclude for this package"
 fi
 
 echo -e "\nDeleting dangling images..."
@@ -33,6 +46,7 @@ while read -r line; do
 	echo Dangling image with ID $id deleted successfully
 done <<< $ids_to_delete
 
-rm -rf $temp_file
+rm -rf $temp_file_exclude
+rm -rf $temp_file_delete
 echo -e "\nAll the dangling images have been removed successfully"
 exit 0
