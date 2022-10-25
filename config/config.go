5758330910a16c9c4f43cae217ce1330bd87b2d3
Alexo <darkonion0@clapot.org>
2022-10-25 08:31:36 +0000

    feat(config/config): add a new setting and reformat others

--- config/config.go
+++ config/config.go
@@ -11,14 +11,35 @@ func init() {
 }
 
 var (
-	Client       *mongo.Client
-	DBName       = flag.String("db", "", "The mongodb database name")
-	DBUrl        = flag.String("url", "", "The mongodb access url")
-	GuildID      = flag.String("guild", "", "Test guild ID. If not passed - bot registers commands.Commands globally")
-	BotToken     = flag.String("token", "", "Bot access token")
+	/*
+		Constant
+	*/
+
+	// The mongodb client configuration
+	Client *mongo.Client
+	// The software/data structure version
+	Version string
+
+	/*
+		Command line flags
+	*/
+
+	// The mongodb database name
+	DBName = flag.String("db", "", "The mongodb database name")
+	// The mongodb connection url
+	DBUrl = flag.String("url", "", "The mongodb access url")
+	// The guild id, not really used or tested
+	GuildID = flag.String("guild", "", "Test guild ID. If not passed - bot registers commands.Commands globally")
+	// The discord bot token
+	BotToken = flag.String("token", "", "Bot access token")
+	// The discord channel id where to post musics
 	ChannelMusic = flag.String("chanm", "", "Discord channel id where the post of the music category will be sent to")
+	// The discord channel id where to post videos
 	ChannelVideo = flag.String("chanv", "", "Discord channel id where the post of the video category will be sent to")
+	// The bot administrators ids
 	BotAdminRole = flag.String("admin", "0", "The bot administrator discord role ID")
-	Debug        = flag.String("debug", "false", "Sets log level to debug true/(false)")
-	Timer        = flag.Int64("timer", 3600000000000, "Set a custom timer scheduled for all the background tasks of the bot, run every [X] nanoseconds")
+	// The global debug varibable, enable shiny logging...
+	Debug = flag.String("debug", "false", "Sets log level to debug true/false")
+	// The timer in nanoseconds for the background task
+	Timer = flag.Int64("timer", 3600000000000, "Set a custom timer scheduled for all the background tasks of the bot, run every [X] nanoseconds")
 )
